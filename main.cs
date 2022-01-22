0e6ebf8e519fd16b0010868fa4dddfce136ac4e6
Jonathan Chandra <7194027+Jonathan-Chandra@users.noreply.github.com>
2022-01-22 04:31:44 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+class Program {
+  public static void Main (string[] args) {
+    // Run Problem 1
+    var one = new One();
+    Console.WriteLine(one.Multiples_of_3_or_5(1000));
+    
+  }
+}
